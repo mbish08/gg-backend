8ba7d1f5810ec84d6260a9d547f045826450280d
mbish08 <mdrew8@gmail.com>
2021-06-04 14:58:11 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# group seeds
+roses = PlantGroup.create(name: 'Roses')
+vegetables = PlantGroup.create(name: 'Vegetables')
+palms = PlantGroup.create(name: 'Palms')
+
+# type seeds
+hotcocoa = PlantType.create(name: 'Hot Cocoa', fert_type: 'Dr Q Rose', water: 'when dry', misc_info: 'deadhead when needed', plant_group_id: roses.id)
+mrlincoln = PlantType.create(name: 'Mr Lincoln', fert_type: 'Dr Q Rose', fert_sched: 'weekly', water: 'lots', misc_info: 'burns with too much sun', plant_group_id: roses.id)
+zuchinni = PlantType.create(name: 'Zuchinni', water: '2x day', soil_ph: 'neutral', soil_type: 'vegetable mix', misc_info: 'deadhead when needed', plant_group_id: vegetables.id)
+
