4d9ffefef7ecc6c44ae433fb23a501f931a860dd
mbish08 <mdrew8@gmail.com>
2021-06-06 16:21:21 -0700

    fleshed out controllers

--- app/controllers/api/v1/plant_groups_controller.rb
+++ app/controllers/api/v1/plant_groups_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::PlantGroupsController < ApplicationController
 
     def index
-        
+        @plant_groups = PlantGroup.all
+        render json: @plant_groups
     end
 
     def create
-
+        @plant_group = PlantGroup.new(plant_group_params)
+        if @plant_group.save
+            render json: @plant_group
+        else
+            render json: {error: 'Error creating new plant group'}
+        end
     end
 
     def show
-    
+        @plant_group = PlantGroup.find(params[:id])
+        render json: @plant_group
     end
 
     def destroy
-
+        @plant_group = PlantGroup.find(params[:id])
+        @plant_group.destroy
     end
 
     private
--- app/controllers/api/v1/plant_types_controller.rb
+++ app/controllers/api/v1/plant_types_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::PlantTypesController < ApplicationController
     
     def index
-        
+        @plant_types = PlantTypes.all
+        render json: @plant_types
     end
 
     def create
-
+        @plant_type = PlantType.new(plant_type_params)
+        if @plant_type.save
+            render json: @plant_type
+        else
+            render json: {error: 'Error creating new plant type'}
+        end
     end
 
     def show
-    
+        @plant_type = PlantType.find(params[:id])
+        render json: @plant_type
     end
 
     def destroy
-
+        @plant_type = PlantType.find(params[:id])
+        @plant_type.destroy
     end
 
     private
