4440d7f2484896f069bd2bca9a554e3ff7cafed1
mbish08 <mdrew8@gmail.com>
2021-06-06 16:07:52 -0700

    basic controller setup

--- app/controllers/api/v1/plant_groups_controller.rb
+++ app/controllers/api/v1/plant_groups_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::PlantGroupsController < ApplicationController
+
+    def index
+        
+    end
+
+    def create
+
+    end
+
+    def show
+    
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def plant_group_params
+        params.require(:plant_group).permit(:name)
+    end
+
 end
--- app/controllers/api/v1/plant_types_controller.rb
+++ app/controllers/api/v1/plant_types_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::PlantTypesController < ApplicationController
+    
+    def index
+        
+    end
+
+    def create
+
+    end
+
+    def show
+    
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def plant_type_params
+        params.require(:plant_type).permit(:name, :fert_type, :fert_sched, :water, :soil_ph, :soil_type, :misc_info, :plant_group_id)
+    end
 end
